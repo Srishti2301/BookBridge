4fa1c68ff85de60814388d9780d7862cc9b6477e
Srishti2301 <1032210688@tcetmumbai.in>
2024-10-18 19:00:20 +0530

    Initial commit for BookBridge project

---
+++ react-app/src/components/Categories.jsx
@@ -0,0 +1,22 @@
+import { Link, useNavigate } from 'react-router-dom';
+import './Header.css';
+
+
+function Categories (props){
+    
+
+    return (
+        <div className='header-container d-flex justify-content-between'>
+           
+            
+
+
+
+            
+        </div>
+    )
+        
+    
+}
+
+export default Categories;
---
+++ react-app/src/components/Home.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'react';
+import Header from './Header';
+import { useNavigate ,Link } from 'react-router-dom';
+import axios from 'axios';
+function Home(){
+
+    const navigate = useNavigate()
+    const [books,setbooks] = useState([]);
+    const [search,setsearch] = useState([]);
+
+    // useEffect(()=>{
+    //     if(!localStorage.getItem('token')){
+    //         navigate('/login')
+    //     }
+
+    // },[])
+    useEffect(()=>{
+        const url = 'http://localhost:4000/get-books'
+            axios.get(url)
+            .then((res)=>{
+                console.log(res)
+                if(res.data.books){
+                    setbooks(res.data.books);
+
+                }
+            })
+            .catch((err)=>{
+                console.log(err)
+                alert('Server Err')
+
+            })
+
+    },[])
+    const handlesearch =(value)=>{
+        setsearch(value);
+    }
+    const handleClick=()=>{
+        console.log('books',books);
+        let filteredBooks= books.filter((item)=>{
+            if(item.bname.toLowerCase().includes(search.toLowerCase()) || item.bdesc.toLowerCase().includes(search.toLowerCase())|| item.category.toLowerCase
+            ().includes(search.toLowerCase())){
+                return item;
+            }
+            
+        })
+        setbooks(filteredBooks)
+
+    }
+    return(
+        <div>
+            <Header search={search} handlesearch={handlesearch }handleClick ={handleClick}/>
+            {!!localStorage.getItem('token') && <Link to="/add-books">ADD BOOKS</Link>}
+            
+            <div className='d-flex justify-content-center flex-wrap'>
+            {books && books.length >0 &&
+              books.map((item,index)=>{
+                return(
+                    <div key={item._id} className='card m-3'>
+                        <img width="300px" height="200px" src={'http://localhost:4000/' + item.bimage}/>
+                        <p className='m-2'>{item.bname} | {item.category}</p>
+                        <h3 className='m-2 text-danger'>{item.price}</h3>
+                        <p className='m-2 text-success'>{item.bdesc}</p>
+                        
+                        
+                        
+
+                    </div>
+              )
+              })}
+              </div>
+        </div>
+    )
+}
+
+export default Home;
